c103c2a40ebd84f4bf6ba019edf3e4ad3a8e9948
Gleans <fuancui@gmail.com>
2019-07-17 21:36:37 +0800

    初始化项目

---
+++ src/main/java/store/zabbix/wheather/SendWheatherMailApplication.java
@@ -0,0 +1,13 @@
+package store.zabbix.wheather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendWheatherMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendWheatherMailApplication.class, args);
+	}
+
+}
